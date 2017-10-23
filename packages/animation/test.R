08fa6ff6bc29f134da1c93ca2bb6525947ef6c9e
OganM <ogan.mancarcii@gmail.com>
2017-10-23 13:38:19 -0700

    pdftk added to animation

--- packages/animation/test.R
+++ packages/animation/test.R
@@ -12,3 +12,17 @@ animation::saveVideo({
 	animation::ani.options(interval = 0.05, nmax = 300)
 	animation::brownian.motion(pch = 21, cex = 5, col = "red", bg = "yellow")
 }, video.name = "BM.mp4", other.opts = "-pix_fmt yuv420p -b 300k")
+
+pathPDFTK = system2('which',args = 'pdftk',stdout = TRUE)
+animation::ani.options(pdftk = pathPDFTK)
+
+pdf('plot.pdf')
+plot(rnorm(4))
+dev.off()
+
+animation::pdftk('plot.pdf', output = 'plot0.pdf')
+if (file.exists("plot0.pdf")) {
+    file.remove("plot0.pdf")
+} else {
+    stop("pdftk does not work")
+}
