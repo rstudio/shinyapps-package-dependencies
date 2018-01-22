1ea1f1316056b20233bdd3b719b4f99e66fd4f36
Joshua Spiewak <jss@rstudio.com>
2018-01-22 08:59:23 -0500

    st_make_valid and st_is_valid extracted to new package
    See r-spatial/sf#537 and https://github.com/r-spatial/lwgeom

--- packages/sf/test.R
+++ packages/sf/test.R
@@ -6,8 +6,3 @@ library(sf)
 x = st_point(0:1)
 y = st_point(3:4)
 st_intersects(x, y)
-
-# st_make_valid:
-x = st_sfc(st_polygon(list(rbind(c(0,0),c(0.5,0),c(0.5,0.5),c(0.5,0),c(1,0),c(1,1),c(0,1),c(0,0)))))
-y = st_make_valid(x)
-st_is_valid(y) # TRUE
