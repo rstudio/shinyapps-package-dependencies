062a961633feab4ce7230d89bec27dbecae4a828
D-SIDD <contact@d-sidd.com>
2018-02-28 22:23:59 +0100

    Update test.R

--- packages/lwgeom/test.R
+++ packages/lwgeom/test.R
@@ -3,6 +3,4 @@ install.packages("lwgeom", repos = "https://cran.rstudio.com")
 
 # Run example from the manual
 library(lwgeom)
-library(sf)
-x = st_sfc(st_polygon(list(rbind(c(0,0),c(0.5,0),c(0.5,0.5),c(0.5,0),c(1,0),c(1,1),c(0,1),c(0,0)))))
-y = lwgeom::st_make_valid(x)
+lwgeom_extSoftVersion()
