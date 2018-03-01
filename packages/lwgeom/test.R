ddd79137072a4cc885f397fcef0932b7521785ec
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2018-03-01 21:18:15 +0100

    add dependencies for lwgeom

---
+++ packages/lwgeom/test.R
@@ -0,0 +1,9 @@
+# Install from CRAN
+install.packages("lwgeom", repos = "https://cran.rstudio.com")
+
+# Run example
+library(sf)
+x = st_point(0:1)
+y = st_point(3:4)
+library(lwgeom)
+st_transform_proj(st_sfc(x, y), c("+proj=longlat", "+init=epsg:3587"))
