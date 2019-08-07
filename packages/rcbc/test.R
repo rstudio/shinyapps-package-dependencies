d16ac25c1fd42ab195fa80d95a21647efde0c6d0
muschellij2 <muschellij2@gmail.com>
2019-08-07 12:26:53 -0400

    added to test because not on cran

--- packages/rcbc/test.R
+++ packages/rcbc/test.R
@@ -1,2 +1,3 @@
 options(download.file.method='curl')
-install.packages('rcbc', repos='https://cran.rstudio.com')
+install.packages('remotes', repos='https://cran.rstudio.com')
+remotes::install_github("dirkschumacher/rcbc")
