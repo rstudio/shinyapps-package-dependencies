8e92ab189ef8db3a6d759c4b59320628acb43807
muschellij2 <muschellij2@gmail.com>
2019-08-07 12:01:40 -0400

    Edited contributin
    added to contributing
    
    added contributing
    
    removed packages not supposed to be there

--- packages/docxtractr/test.R
+++
@@ -1,8 +0,0 @@
-options(download.file.method = "curl")
-install.packages("docxtractr", repos = "https://cran.rstudio.com")
-
-destfile = tempfile(fileext = ".doc")
-dl = download.file(url = "http://www.iiswc.org/iiswc2013/sample.doc",
-                   destfile = destfile)
-
-res = docxtractr::read_docx(destfile)
