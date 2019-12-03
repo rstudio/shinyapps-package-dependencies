fb18db19c0fcd1a38a976947ceb46decca270b30
Bronsonk0619 <bronsonk0619@gmail.com>
2019-12-03 12:34:48 -0500

    Testing ChemmineOB package.

--- packages/openbabel/test.R
+++ packages/openbabel/test.R
@@ -2,3 +2,5 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager", repos = "http://cran.rstudio.com/")
 
 BiocManager::install("ChemmineOB")
+molRefs = forEachMol("SMILES","C1CCCCC1\ttest-compound-name",identity)
+fingerprint_OB(molRefs,"FP3")
