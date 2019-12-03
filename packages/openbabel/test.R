af518ef07e53d44f82587f9f58bb1597f0e19aec
Bronsonk0619 <bronsonk0619@gmail.com>
2019-12-03 12:40:49 -0500

    Added ChemmineOB:: before package's functions.

--- packages/openbabel/test.R
+++ packages/openbabel/test.R
@@ -2,5 +2,5 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager", repos = "http://cran.rstudio.com/")
 
 BiocManager::install("ChemmineOB")
-molRefs = forEachMol("SMILES","C1CCCCC1\ttest-compound-name",identity)
-fingerprint_OB(molRefs,"FP3")
+molRefs = ChemmineOB::forEachMol("SMILES","C1CCCCC1\ttest-compound-name",identity)
+ChemmineOB::fingerprint_OB(molRefs,"FP3")
