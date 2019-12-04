3bdedd10a4dcfc2d1dcc417ce25397a43de2cb0c
Romain Lesur <romain.lesur@gmail.com>
2019-12-04 02:20:29 +0100

    test only the system dependencies, see https://github.com/rstudio/shinyapps-package-dependencies/pull/220#discussion_r353306773

--- packages/pagedown/test.R
+++ packages/pagedown/test.R
@@ -3,17 +3,3 @@ install.packages("pagedown", repos = "https://cran.rstudio.com", type = "source"
 
 # Test against Chromium installation
 pagedown::find_chrome()
-
-# Functional test (Pandoc is required)
-source_file <- system.file(
-  "rmarkdown/templates/html-paged/skeleton/skeleton.Rmd",
-  package = "pagedown",
-  mustWork = TRUE
-)
-
-pagedown::chrome_print(
-  source_file, 
-  tempfile(), 
-  extra_args = c("--disable-gpu", "--no-sandbox"), 
-  verbose = 1
-)
