131a39b7ecaaa407381d37abb2cd6c7760bb1591
Joshua Spiewak <jss@rstudio.com>
2020-05-19 14:36:50 -0400

    More tweaks for RSelenium
    - `caTools` head version now requires R > 3.6.0
    - functions of RSelenium package are slightly different

--- packages/RSelenium/test.R
+++ packages/RSelenium/test.R
@@ -1,4 +1,4 @@
 options(download.file.method="curl")
 install.packages("RSelenium", repos="https://cran.rstudio.com")
-pJS <- RSelenium::phantom()
-pJS$stop()
+rD <- RSelenium::rsDriver(browser = "phantomjs")
+rD[["server"]]$stop()
