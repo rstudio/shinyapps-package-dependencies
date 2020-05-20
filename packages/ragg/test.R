34bb2df3a1766bb9779aa9b91e46286fe34f6ade
Carson <cpsievert1@gmail.com>
2020-05-20 13:25:20 -0500

    Add libjpeg (recent addition) and more tests

--- packages/ragg/test.R
+++ packages/ragg/test.R
@@ -6,3 +6,16 @@ ragg::agg_png(tmpfile)
 plot(1:10)
 dev.off()
 unlink(tmpfile, recursive = TRUE)
+
+tmpfile <- tempfile(fileext = ".tiff")
+ragg::agg_tiff(tmpfile)
+plot(1:10)
+dev.off()
+unlink(tmpfile, recursive = TRUE)
+
+
+tmpfile <- tempfile(fileext = ".jpeg")
+ragg::agg_jpeg(tmpfile)
+plot(1:10)
+dev.off()
+unlink(tmpfile, recursive = TRUE)
