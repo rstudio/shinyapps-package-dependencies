3668b89b7d14b86ec695456506fe0b7396cde907
Florian Kohrt <fkohrt@anche.no>
2021-03-02 11:17:50 +0100

    Delete files afterwards

--- packages/tinytex/test.R
+++ packages/tinytex/test.R
@@ -1,8 +1,12 @@
 options(download.file.method = "curl")
 install.packages("tinytex", repos = "https://cran.rstudio.com")
 tinytex::install_tinytex()
-tinytex::tlmgr_install(c("svg", "koma-script", "trimspaces", "ifplatform", "catchfile", "transparent", "fp", "ms", "pgf"))
+tinytex::tlmgr_install(c("svg", "koma-script", "trimspaces",
+                         "ifplatform", "catchfile", "transparent",
+                         "fp", "ms", "pgf"))
 tinytex::tlmgr_update()
+download.file(url = "https://upload.wikimedia.org/wikipedia/commons/8/84/Example.svg",
+              destfile = "test.svg")
 tex_document <- "
 \\documentclass{article}
 \\usepackage[inkscapearea=page]{svg}
@@ -10,7 +14,7 @@ tex_document <- "
 \\includesvg{test}
 \\end{document}
 "
-download.file(url = "https://upload.wikimedia.org/wikipedia/commons/8/84/Example.svg", destfile = "test.svg")
 write(tex_document, file = "test.tex")
 tinytex::latexmk(file = "test.tex", engine_args = "-shell-escape")
-
+file.remove("test.svg")
+file.remove("test.tex")
