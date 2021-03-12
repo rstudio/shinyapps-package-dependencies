43f97de75edaf3a9b2d52cd1cf0d110b3690d40a
Joshua C. Forest <josh.forest@rstudio.com>
2021-03-12 11:59:34 -0500

    do not support docxtractr on focal

--- packages/docxtractr/test.R
+++ packages/docxtractr/test.R
@@ -1,3 +1,6 @@
+if (system('lsb_release -sr', intern=TRUE) == "20.04") {
+print("docxtractr is not supported on 20.04 due to https://github.com/rstudio/shinyapps-package-dependencies/issues/282")
+} else {
 install.packages("docxtractr", repos = "https://cran.rstudio.com")
 
 mytempdir = tempdir()
@@ -6,3 +9,4 @@ dl = download.file(url = "http://www.iiswc.org/iiswc2013/sample.doc", destfile =
 
 docxtractr:::lo_find()
 res = docxtractr::read_docx(mydestfile)
+}
