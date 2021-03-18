93362ed527b3e60672070ee5b93ab752d80fdf8b
Barret Schloerke <schloerke@gmail.com>
2021-03-18 11:44:49 -0400

    Use local lib path for pak that is ignored for regular execution

--- pak/install_pkg.R
+++ pak/install_pkg.R
@@ -1,10 +1,31 @@
+# Returns the file currently being sourced or run with Rscript
+thisFile <- function() {
+  cmdArgs <- commandArgs(trailingOnly = FALSE)
+  needle <- "--file="
+  match <- grep(needle, cmdArgs)
+  if (length(match) > 0) {
+    # Rscript
+    return(normalizePath(sub(needle, "", cmdArgs[match])))
+  } else {
+    # 'source'd via R console
+    return(normalizePath(sys.frames()[[1]]$ofile))
+  }
+}
+# set to `pak` folder
+setwd(dirname(thisFile()))
+
+os <- tryCatch({
+  system("lsb_release -sc", intern = TRUE)
+}, error = function(e) {
+  "local"
+})
 
 args <- commandArgs(TRUE)
 if (length(args) > 0) {
   pkgs <- args[-1]
 
   # Must match install file
-  lib <- file.path("pak", "lib", R.Version()$platform, paste0(R.Version()$major, ".", R.Version()$minor))
+  lib <- file.path("lib", os, paste0(R.Version()$major, ".", R.Version()$minor))
   # Load pak from our tmp lib
   require("pak", character.only = TRUE, lib.loc = lib, quietly = TRUE)
 
--- pak/setup.R
+++ pak/setup.R
@@ -1,5 +1,27 @@
+# Returns the file currently being sourced or run with Rscript
+thisFile <- function() {
+  cmdArgs <- commandArgs(trailingOnly = FALSE)
+  needle <- "--file="
+  match <- grep(needle, cmdArgs)
+  if (length(match) > 0) {
+    # Rscript
+    return(normalizePath(sub(needle, "", cmdArgs[match])))
+  } else {
+    # 'source'd via R console
+    return(normalizePath(sys.frames()[[1]]$ofile))
+  }
+}
+# set to `pak` folder
+setwd(dirname(thisFile()))
+
+os <- tryCatch({
+  system("lsb_release -sc", intern = TRUE)
+}, error = function(e) {
+  "local"
+})
+
 # Must match install file
-lib <- file.path("pak", "lib", R.Version()$platform, paste0(R.Version()$major, ".", R.Version()$minor))
+lib <- file.path("lib", os, paste0(R.Version()$major, ".", R.Version()$minor))
 if (!dir.exists(lib)) {
   dir.create(lib, recursive = TRUE)
 }
@@ -7,6 +29,9 @@ if (!dir.exists(lib)) {
 if (system.file(package = "pak", lib.loc = lib) == "") {
   # install latest
   install.packages("pak", repos = "https://r-lib.github.io/p/pak/dev/", lib = lib)
+  # quick test
+  require("pak", character.only = TRUE, lib.loc = lib, quietly = FALSE)
+
 } else {
   # Load pak from our tmp lib
   require("pak", character.only = TRUE, lib.loc = lib, quietly = TRUE)
