d9dfbec205907efd44bbe3b17fa799a1cf091e5c
David <davidmfstein@gmail.com>
2022-03-13 19:19:25 -0400

    bedr support

---
+++ packages/bedr/test.R
@@ -0,0 +1,10 @@
+options(download.file.method="curl")
+install.packages('bedr', repos="https://cran.rstudio.com")
+install.packages('testit', repos="https://cran.rstudio.com")
+
+library(bedr)
+library(testit)
+
+assert(check.binary(x = "bedtools", verbose = F))
+assert(check.binary(x = "bedops", verbose = F))
+assert(check.binary(x = "tabix", verbose = F))
