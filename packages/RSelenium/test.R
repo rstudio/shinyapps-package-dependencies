dc8d200e757d47d2fd6148ce485b6adfbb062a91
Sam Perman <sam@posit.co>
2022-12-21 14:26:49 -0500

    no not install chrome

--- packages/RSelenium/test.R
+++ packages/RSelenium/test.R
@@ -1,4 +1,4 @@
 options(download.file.method="curl")
 install.packages("RSelenium", repos="https://cran.rstudio.com")
-rD <- RSelenium::rsDriver(browser = "phantomjs", geckover = NULL, verbose = TRUE)
+rD <- RSelenium::rsDriver(browser = "phantomjs", geckover = NULL, chromever = NULL)
 rD[["server"]]$stop()
