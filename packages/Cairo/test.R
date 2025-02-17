47e3f21ecb18b2e1778be5a8f0b2ef0b688ac9da
Michael Mayer <michael.mayer@posit.co>
2025-02-17 09:39:02 +0100

    fixing Cairo

--- packages/Cairo/test.R
+++ packages/Cairo/test.R
@@ -3,8 +3,8 @@ install.packages("Cairo", repos = "https://cran.rstudio.com")
 
 library("Cairo")
 
-CairoWin(5,5)
+Cairo(5,5)
 
-CairoPNG(filename = "c:/Users/yourname/nice.png", width = 600, height = 600)
+CairoPNG(filename = "nice.png", width = 600, height = 600)
 
 dev.off()
