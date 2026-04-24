5a4e72b9c4da81d76960d85d00be6b29497a61dd
Sam Perman <sam@posit.co>
2026-04-24 07:53:26 -0400

    fix the remaining test failures
    rSelenium depended on phantomjs which is long since dead
    rBlast depended on devtools which has a few new system deps unrelated to rBlast so stop using it

--- packages/RSelenium/test.R
+++ packages/RSelenium/test.R
@@ -1,4 +1,7 @@
 options(download.file.method="curl")
 install.packages("RSelenium", repos="https://cran.rstudio.com")
-rD <- RSelenium::rsDriver(browser = "phantomjs", geckover = NULL, chromever = NULL)
-rD[["server"]]$stop()
+rD <- RSelenium::remoteDriver(
+  remoteServerAddr = "localhost",
+  port = 4445L,
+  browserName = "firefox"
+)
